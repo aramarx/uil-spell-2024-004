ad042416746dfa6db42deaa331d624b5a3859f36
aramarx <aramarx.us@gmail.com>
2024-12-05 14:29:23 -0600

    Update dispay.py

--- pages/dispay.py
+++ pages/dispay.py
@@ -8,13 +8,6 @@ import pickle
 import datetime
 import time
 
-DIFF_JST_FROM_UTC = 9
-now = datetime.datetime.utcnow() + datetime.timedelta(hours=DIFF_JST_FROM_UTC)
-
-ut_now = time.time()
-
-f = open('./list.txt','rb')
-
 def main():
 	    
     st.title("UIL Dashboard")
