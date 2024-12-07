daab7dfe6fe532410c10c874f273f049069bb658
aramarx <aramarx.us@gmail.com>
2024-12-06 19:37:39 -0600

    Update main.py

--- main.py
+++ main.py
@@ -2,17 +2,12 @@ import streamlit as st
 import pickle
 import datetime
 import time
-
 import streamlit_authenticator as stauth
 import yaml
-
-######################
 
 # Define timezone difference
 DIFF_JST_FROM_UTC = 9
 ent_time = datetime.datetime.utcnow() + datetime.timedelta(hours=DIFF_JST_FROM_UTC)
-
-start = time.time() 
 
 # Load configuration
 with open('config.yaml') as file:
@@ -30,25 +25,20 @@ authenticator = stauth.Authenticate(
 # User login process
 name, authentication_status, username = authenticator.login('main', 'main')
 
-
 if 'authentication_status' not in st.session_state:
     st.session_state['authentication_status'] = None
 
-if st.session_state["authentication_status"]:
+if authentication_status:
+    # Successful login, redirect to the next page
     authenticator.logout('Logout', 'main')
     st.write(f'Login successful')
-		
-  
-        #After login process
+    
+    # Add redirect to the page after login
+    st.session_state['authenticated'] = True
+    st.experimental_rerun()
 
-    def main():
-	    
-    	st.title("UIL Dashboard")
-
-    	st.page_link("UIL_dashboard.py", label="Back to home page")
-	
-        ####
-elif st.session_state["authentication_status"] is False:
+elif authentication_status is False:
     st.error('The username or password is incorrect')
-elif st.session_state["authentication_status"] is None:
-    st.warning('Please enter your username and password') 
+
+elif authentication_status is None:
+    st.warning('Please enter your username and password')
