43520b6767555f0cb1952ca228a271f2cff74498
aramarx <aramarx.us@gmail.com>
2024-12-08 16:28:31 -0600

    Update main.py

--- main.py
+++ main.py
@@ -37,6 +37,7 @@ if 'authentication_status' not in st.session_state:
 if st.session_state["authentication_status"]:
     # Successful login, redirect to the next page
     authenticator.logout('Logout', 'main')
+    st.experimental_rerun()
     st.write(f'Login successful')		
 
 	
